8ff4fc3dc1a3a14baa462a200c320668d020dd60
jborbely <joe.borbely@gmail.com>
2020-07-15 17:21:27 +1200

    remove Python 2.7 from classifiers

--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ setup(
         'Environment :: Console',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
