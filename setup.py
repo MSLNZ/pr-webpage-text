fcf2331a4e8a104f02c90109878612f97df228b3
jborbely <joe.borbely@gmail.com>
2022-08-06 09:20:30 +1200

    include Python 3.10 in classifiers

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     install_requires=['flask', 'gevent', 'requests'],
     packages=['webpage_text'],
